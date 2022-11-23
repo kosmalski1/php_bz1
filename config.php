b46837f1c97079504a66ddc7f74ad5cf8b7abc81
kosmalski1 <pawelekk0505@gmail.com>
2022-11-23 13:36:05 +0100

    twigconfig

--- config.php
+++ config.php
@@ -1,4 +1,7 @@
 <?php
+require_once('vendor/autoload.php');
+$loader = new Twig\Loader\FilesystemLoader('templates');
+$twig = new Twig\Environment($loader);
 $db = new mysqli('localhost', 'root', '', 'uzytkownicy');
 
 ?>
